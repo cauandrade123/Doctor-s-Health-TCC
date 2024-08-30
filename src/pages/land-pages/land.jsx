7ff801b193daecd99ff85a4fae546b088169fa16
thiago-vieira2 <ra58029747888@acaonsfatima.org.br>
2024-08-30 16:29:42 -0300

    TCC

--- src/pages/land-pages/land.jsx
+++ src/pages/land-pages/land.jsx
@@ -1,9 +1,19 @@
 import Header from "../../components/header/header";
 import './land.scss'
+import Medico from "../../assets/img/tcc/tccassests/land pages/aquinorego.svg"
+import Retangulo from "../../assets/img/tcc/tccassests/land pages/retangulo_landpagge.svg"
+
+
+
 export default function Land () {
     return (
         <div className="mae">
           <Header/>
+
+
+      <div className="Texto-image">
+        
+
 
           <div className="informaçoes-do-doutor">
             <h1>Dr. João Silva</h1>
@@ -12,9 +22,18 @@ export default function Land () {
 
 
             <div className="A-da-info">   <a href="">Agendar <p>consulta.</p></a></div>
+
+
+          
          
          
           </div>
+
+          <div className="img-doutor">
+            <img src={Medico} alt="" />
+          </div>
+          
+        </div>
         </div>
     )
 }

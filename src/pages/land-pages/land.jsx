040bf23d58d797c0f488fa8c18f9c5730d8f683a
voltareli01 <voltstrem235@gmail.com>
2024-08-31 00:00:54 -0300

    upado

--- src/pages/land-pages/land.jsx
+++ src/pages/land-pages/land.jsx
@@ -1,8 +1,8 @@
 import Header from "../../components/header/header";
 import './land.scss'
-import Medico from "../../assets/img/tcc/tccassests/land pages/aquinorego.svg"
-import Retangulo from "../../assets/img/tcc/tccassests/land pages/retangulo_landpagge.svg"
-import Wave from "../../assets/img/tcc/tccassests/land pages/wave 4.svg"
+import Medico from "../../assets/img/tcc/tccassests/landpages/aquinorego.svg"
+import Retangulo from "../../assets/img/tcc/tccassests/landpages/retangulo_landpagge.svg"
+import Wave from "../../assets/img/tcc/tccassests/landpages/wave 4.svg"
 
 
 export default function Land () {
@@ -14,7 +14,7 @@ export default function Land () {
                       <h1>Dr. João Silva</h1>
                       <h2>clinico geral, feito para solucionar seus problemas.   </h2>
                   <div className="A-da-info"> <a href="a">Agendar <p>consulta.</p> </a> </div>
-                  </div>  voltareli
+                  </div> 
                   <div className="img-doutor">
                       <img src={Medico} alt="" />
                   </div>

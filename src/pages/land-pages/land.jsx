e434e6e2157c12cc61f7fffa6ffb73f61ce454b3
Cauã Andrade <caua.goldenboyy@gmail.com>
2024-08-31 00:49:25 -0300

    seção 2 em andamento

--- src/pages/land-pages/land.jsx
+++ src/pages/land-pages/land.jsx
@@ -1,9 +1,11 @@
 import Header from "../../components/header/header";
 import './land.scss'
 import Medico from "../../assets/img/tcc/tccassests/land pages/aquinorego.svg"
-import Retangulo from "../../assets/img/tcc/tccassests/land pages/retangulo_landpagge.svg"
 import Wave from "../../assets/img/tcc/tccassests/land pages/wave 4.svg"
-
+import iconSaibaMais from '../../assets/img/tcc/tccassests/simbolos/saiba mais.svg'
+import Facebook from '../../assets/img/tcc/tccassests/simbolos/Facebook.svg'
+import instagram from '../../assets/img/tcc/tccassests/simbolos/Instagram preto.svg'
+import Twitter from '../../assets/img/tcc/tccassests/simbolos/twitter.svg'
 
 export default function Land () {
     return (
@@ -14,12 +16,39 @@ export default function Land () {
                       <h1>Dr. João Silva</h1>
                       <h2>clinico geral, feito para solucionar seus problemas.   </h2>
                   <div className="A-da-info"> <a href="a">Agendar <p>consulta.</p> </a> </div>
-                  </div>  ve se foiiiii
+                  </div>  
                   <div className="img-doutor">
                       <img src={Medico} alt="" />
                   </div>
             </div>
           <img src={Wave} alt="" />  
-        </div>
+          <section className='secao-2'>
+            <div className="main">
+                <div className="saibaMais">
+                      <div className="iconeEtexto">
+                            <img className='iconeSaibaMais' src={iconSaibaMais} alt="" />
+                            <h2>Saiba Mais</h2>
+                      </div> 
+                      <div className="textoSaibaMais">
+                          <p>
+                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt consectetur veniam ut reprehenderit in, minima sunt molestias dolorum nihil est at dolorem rerum pariatur delectus necessitatibus autem dignissimos quasi repellat.
+                          </p>
+                      </div>
+                </div>
+                <div className="conhecaMe">
+                      <div className="textoConhecaMe">
+                            <h2>
+                                conheca me
+                            </h2>
+                      </div>
+                      <div className="iconesConhecaMe">
+                          <img src={Twitter} alt="" />
+                          <img src={instagram} alt="" />
+                          <img src={Facebook} alt="" />
+                      </div> 
+                </div>
+            </div>
+        </section>
+        </div>   
     )
 }

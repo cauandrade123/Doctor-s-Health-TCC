39bc7070a98d16b8b2e08296795a08dbc1306a6a
Pedro <ra45088400861@acaonsfatima.org.br>
2024-09-28 11:30:07 -0300

    pagina adm arrumada pra ficar bom nas nossas telas

--- src/pages/adm/adm.jsx
+++ src/pages/adm/adm.jsx
@@ -4,6 +4,7 @@ import logo from "../../assets/img/tcc/tccassests/logo/logomedica.svg"
 import './adm.scss'
 import { useState } from "react";
 import Cardadm from "../../components//card-pagina-adm/consultas"
+import Seta from '../../assets/img/tcc/tccassests/simbolos/SetaVoltarADM.svg'
 
 export default function Adm() {
 
@@ -17,13 +18,12 @@ export default function Adm() {
     }
 
     return (
-        <main>
+        <div className="main-adm">
             <div className="menu-adm">
                 <div className="logo-e-voltar">
                     <img src={logo} alt="logo" />
-                    <Link className="voltar" to='/' >voltar</Link>
+                    <Link className="voltar" to='/' ><img src={Seta} alt="" /></Link>
                 </div>
-
                 <div className="menu-azul">
                     <div className="opcoes-do-menu">
                         <button  className="Agenda">Agenda</button>
@@ -37,6 +37,6 @@ export default function Adm() {
             <div className="consultas">
                 {conteudo}
             </div>
-        </main>
+        </div>
     );
 }

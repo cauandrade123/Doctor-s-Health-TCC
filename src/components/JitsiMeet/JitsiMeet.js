ea57975ac8d5a26a32cf9fe208b5a7cf3e44038a
Pedro <pedroeinviller142@gmail.com>
2024-11-01 17:38:20 -0300

    comitei o jt

--- src/components/JitsiMeet/JitsiMeet.js
+++ src/components/JitsiMeet/JitsiMeet.js
@@ -1,41 +1,35 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 
-
-export default function JitsiMeet({ nm_Sala}) {
-
-    const [link, setLink] = useState('')
+export default function JitsiMeet({ nm_Sala }) {
+    const [link, setLink] = useState('');
 
     useEffect(() => {
+        const dominio = 'meet.jit.si'; 
+        const jwtToken = 'tokenSavede'; 
 
-           const dominio = 'meet.jit.si'
-           const opcoes = {
+        const opcoes = {
             roomName: nm_Sala,
             width: "100%",
             parentNode: document.querySelector('#div-JitsiMeet'),
             configOverwrite: {
                 startWithAudioMuted: true,
-                startWithVideoMuted: false
+                startWithVideoMuted: false,
+                openBridgeChannel: 'websocket'
             },
             interfaceConfigOverwrite: { SHOW_JITSI_WATERMARK: true },
             userInfo: { 
                 displayName: 'Dr. Silva'
-             }
-           };
+            },
+            jwt: jwtToken // Adicione o token JWT aqui
+        };
 
-           const api = new window.JitsiMeetExternalAPI(dominio, opcoes);
+        const api = new window.JitsiMeetExternalAPI(dominio, opcoes);
 
+        setLink(`https://${dominio}/${nm_Sala}`);
+        localStorage.setItem('link', `https://${dominio}/${nm_Sala}`);
         
-    
-           setLink(`https://${dominio}/${nm_Sala}`)
-
-           localStorage.setItem('link', link);
-           console.log(link)
-           
-           return () => api.dispose();
-        }, [nm_Sala]);
-
-
-    
+        return () => api.dispose();
+    }, [nm_Sala]);
 
     return <div id="div-JitsiMeet" style={{ width: '100%', height: '100vh' }}></div>;
 }
